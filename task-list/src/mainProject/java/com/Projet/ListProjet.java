f0b6c0044ec45c49333c5f4563ddb4d406e12531
Arnaud <sibade.arnaud@gmail.com>
2017-01-13 15:01:36 +0100

    Ajout de Projet.afficher et de listProjet;afficher
    ajout de Projet.afficher et de listProjet;afficher

--- task-list/src/mainProject/java/com/Projet/ListProjet.java
+++ task-list/src/mainProject/java/com/Projet/ListProjet.java
@@ -5,4 +5,20 @@ import java.util.ArrayList;
 
 public class ListProjet {
 	public ArrayList<Projet>listProjet=new ArrayList<Projet>();
+	
+	public void addProject(Projet project){
+		listProjet.add(project);
+	}
+	
+	public Projet getProjet(int index){
+		return listProjet.get(index);
+	}
+	
+	public String toString(){
+		String maList = "";
+		for(int i = 0; i<listProjet.size();i++){
+			maList = listProjet.get(i).afficher()+"\n";
+		}
+		return maList;
+	}
 }
--- task-list/src/mainProject/java/com/Projet/Projet.java
+++ task-list/src/mainProject/java/com/Projet/Projet.java
@@ -15,6 +15,7 @@ public class Projet {
 		this.nom=nom;
 	}
 	
+	
 	public String getNom(){
 		return nom;
 	}
